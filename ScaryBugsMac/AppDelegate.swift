83c06383bbee0083764c3100a8e1a874c8da8a16
zachprice <zachprice@purdue.edu>
2015-09-06 17:45:01 -0400

    Initial Commit

---
+++ ScaryBugsMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScaryBugsMac
+//
+//  Created by Zach Price on 9/6/15.
+//  Copyright (c) 2015 Zach Price. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
